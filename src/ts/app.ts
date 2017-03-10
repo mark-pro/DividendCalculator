1ee7b9cdbf15b9d0812dbc0c75ad08dd9d303ef8
Mark Provenzano <mark.pro@stratuscube.com>
2017-03-10 00:15:12 -0500

    added extra props

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,7 +1,7 @@
 import { DividendCalc as DC } from "./divcalc"
 class Startup {
     public static main(): void {
-        var dc = new DC(12.5 , 200);
+        var dc = new DC(12.5 , 200 , 4 , 2);
         document.body.innerHTML = dc.toString();
     }
 }
--- src/ts/divcalc.ts
+++ src/ts/divcalc.ts
@@ -1,18 +1,39 @@
-class DividendCalc {
-    SharePrice: number;
-    BaseNumberOfShares: number;
-
-    constructor(sharePrice: number, baseNumberOfShares: number) {
-        this.SharePrice = sharePrice;
-        this.BaseNumberOfShares = baseNumberOfShares;
+class Result {
+    numberOfShares: string;
+    folioValue: number;
+    
+    constructor(numberOfShares: string, folioValue: number) {
+        this.numberOfShares = numberOfShares;
+        this.folioValue = folioValue;
     }
+}
+
+class DividendCalc {
+    sharePrice: number;
+    baseNumberOfShares: number;
+    dividendPercentage: number;
+    numberOfYears: number;
+    results: Result[]; 
+
+    constructor(
+        sharePrice: number,
+        baseNumberOfShares: number,
+        dividendPercentage: number,
+        numberOfYears: number) {
+        
+        this.sharePrice = sharePrice;
+        this.baseNumberOfShares = baseNumberOfShares;
+        this.dividendPercentage = dividendPercentage;
+        this.numberOfYears = numberOfYears;
+    }
+
+
 
     public print() {
         console.log(this.toString());
     }
 
-    public toString = () => 
-        `{ sharePrice: ${this.SharePrice}, baseNumberOfShares: ${this.BaseNumberOfShares} }`
+    public toString = () => JSON.stringify(this);
 }
 
 export { DividendCalc };
