68c039271030b0d8e26b278cfc199c3910c16069
Mark Provenzano <mark.pro@stratuscube.com>
2017-03-10 01:59:36 -0500

    the math is right, needs clean up and floating point fixes

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -2,6 +2,7 @@ import { DividendCalc as DC } from "./divcalc"
 class Startup {
     public static main(): void {
         var dc = new DC(12.5 , 200 , 4 , 2);
+        dc.start();
         document.body.innerHTML = dc.toString();
     }
 }
--- src/ts/divcalc.ts
+++ src/ts/divcalc.ts
@@ -7,7 +7,6 @@ class Result {
         this.numberOfShares = numberOfShares;
         this.remainingMoney = remainingMoney;
         this.folioValue = this.numberOfShares * pricePerShare + remainingMoney;
-        this.folioValue = Round(this.folioValue , 2);
     }
 
     public toString = () => JSON.stringify(this);
@@ -18,37 +17,43 @@ class DividendCalc {
     baseNumberOfShares: number;
     dividendPercentage: number;
     numberOfYears: number;
-    results: Result[]; 
+    results = new Array<Result>(); 
 
     constructor(
         sharePrice: number,
         baseNumberOfShares: number,
         dividendPercentage: number,
         numberOfYears: number) {
-        
+            
         this.sharePrice = sharePrice;
         this.baseNumberOfShares = baseNumberOfShares;
         this.dividendPercentage = dividendPercentage;
         this.numberOfYears = numberOfYears;
     }
 
-    public Start() {
+    public start() {
         let dividend = this.baseNumberOfShares * this.sharePrice * this.dividendPercentage / 100;
-        dividend = Round(dividend , 2);
+        dividend = Round(dividend, 2);
+        console.log(dividend);
         let newShares = Math.floor(dividend / this.sharePrice);
         let remainingMoney = Round(dividend % this.sharePrice, 2);
         let result = new Result(this.baseNumberOfShares + newShares, remainingMoney, this.sharePrice);
         this.results.push(result);
-        this.Calculate(result, this.numberOfYears)
+        this.calculate(result, this.numberOfYears - 1);
     }
 
-    private Calculate(result: Result, count: number) {
-        if (count > 0) {
-
-
-        }
-
-        count--;        
+    private calculate(result: Result, count: number) {
+        let total = result.remainingMoney + (this.sharePrice * result.numberOfShares);
+        let dividend = total * (this.dividendPercentage / 100);
+        let newShares = Math.floor(dividend / this.sharePrice);
+        let remainingMoney = Round(dividend % this.sharePrice, 2);
+        let newResult = new Result(
+            newShares + result.numberOfShares,
+            remainingMoney,
+            this.sharePrice);
+        this.results.push(newResult);
+        if (--count > 0)
+            this.calculate(newResult, count);
     }
 
     public print() {
